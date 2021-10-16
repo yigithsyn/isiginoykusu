545a6969167e9ee58b48d646ac263756a4a1e8b7
Hüseyin YİĞİT <iletisim@huseyinyigit.com.tr>
2021-10-16 08:45:49 +0300

    seperate data in json

---
+++ js/data.js
@@ -0,0 +1,50 @@
+var data = {
+  "title": {
+    "media": {
+      "url": "https://upload.wikimedia.org/wikipedia/commons/9/97/Shahab_al-Din_Suhrawardi.jpg",
+      "credit": "Wikipedia/<a href='https://tr.wikipedia.org/wiki/%C5%9Eihabeddin_S%C3%BChreverd%C3%AE'>Şihabeddin Sühreverdi</a>"
+    },
+    "text": {
+      "headline": "Şİhabeddİn Sühreverdİ<br/> 1145 - 1191",
+      "text": "<p>İran'lı filozof ve İşrak akımının kurucusu.</p>"
+    }
+  },
+  "events": [
+    {
+      "unique_id": "ccc",
+      "media": {
+        "url": "https://youtu.be/fSrO91XO1Ck",
+        "caption": "",
+        "credit": "<a href=\"http://unidiscmusic.com\">Unidisc Music</a>"
+      },
+      "start_date": {
+        "year": "980"
+      },
+      "end_date": {
+        "year": "1037"
+      },
+      "text": {
+        "headline": "İbn-i Sina (Avicenna)",
+        "text": "Optik alanında bir nesne ışın kuramcısı olarak İbn-i Sina; Razi ve Farabi'nin düşüncelerini temellendirerek, öne sürdüğü anti-tezlerle lerle Eukleidesçi anlayışı çürütmeye çalışmış, bir yandan Aristoteles'in nesne ışın kuramını desteklemiştir. "
+      }
+    },
+    {
+      "unique_id": "ibn-ul-heysem",
+      "media": {
+        "url": "https://www.matematiksel.org/wp-content/uploads/2019/05/heysem.jpg",
+        "caption": "",
+        "credit": "<a href=\"https://www.matematiksel.org/ilk-modern-bilim-insani-ibn-i-heysem\">Matematiksel</a>"
+      },
+      "start_date": {
+        "year": "965"
+      },
+      "end_date": {
+        "year": "1039"
+      },
+      "text": {
+        "headline": "İbn-ül Heysem (El-Haytham)",
+        "text": "Optik alanında bir nesne ışın kuramcısı olarak İbn-i Sina; Razi ve Farabi'nin düşüncelerini temellendirerek, öne sürdüğü anti-tezlerlelerle Eukleidesçi anlayışı çürütmeye çalışmış, bir yandan Aristoteles'in nesne ışın kuramını desteklemiştir."
+      }
+    }
+  ]
+}
