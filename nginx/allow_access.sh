cc7344023b9624c7c827ede7bb55fcffa237eb39
Hüseyin YİĞİT <iletisim@huseyinyigit.com.tr>
2021-10-23 10:54:08 +0300

    reconfigure for new server

--- nginx/allow_access.sh
+++ nginx/allow_access.sh
@@ -2,8 +2,6 @@
 gpasswd -a www-data root
 chmod g+x /root
 chmod g+x /root/sites
-chmod g+x /root/sites/huseyinyigit.com.tr/isiginoykusu.huseyinyigit.com.tr
-chmod g+x /root/html
-chmod g+x /root/html/themes
-chmod g+x /root/html/themes/startbootstrap-resume
+chmod g+x /root/sites/huseyinyigit.com.tr/
+chmod g+x /root/sites/huseyinyigit.com.tr/isiginoykusu
 nginx -s reload
